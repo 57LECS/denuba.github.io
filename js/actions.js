356d797ac9fe7bd3f5149f49fa2f345d86b9dddd
Luis Edgardo Calderon <eddsc57@gmail.com>
2017-05-31 12:39:40 -0500

    Falta un dia

--- js/actions.js
+++ js/actions.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
-	$("#newMsg").hide();
-	$("#typeMsg").hide();
-	$("#msgCliente").hide();  
+	 
 	var dt = new Date();
 	var time = dt.getHours() + ":" + dt.getMinutes();
 	var mail;
 	$("#txtHr1").text(time);
 	$("#btnMoSu").click(function(){
-        $("#msgCliente").show();        
 		var name = $("#txtnameS").val();
 		mail = $("#inputEmail3S").val();
-		$("#msgAut").text(name);
-		$("#modalSugerencia").modal("toggle");
-		$("#btnDemoEnviarQu").hide();
+		if (name != "" && mail != "") {
+			$("#msgCliente").show();        
+			$("#msgAut").text(name);
+			$("#modalSugerencia").modal("toggle");
+			$("#btnDemoEnviarQu").hide();
+			name = $("#txtnameS").text("");
+			mail = $("#inputEmail3S").text("");
+		} else {
+			$("#txtNoti").text("Porfavor ingresa la informacion solicitada");
+		}
 	})
 
 	$("#btnDemoEnviarQu").click(function(){
@@ -29,5 +33,26 @@ $(document).ready(function(){
         $("#newMsg").show();
 	})
 
+	$("#btnFinSu").click(function(){
+		var dt = new Date();
+		var time1 = dt.getHours() + ":" + dt.getMinutes()
+		var name = $("#msgAut").val();
+		var sug = $("#txtMsg1").val();
+		if (name != "" && sug != "") {
+			$("#frmNuevo").show();        
+			$("#txtNewUsrNom").text(name);
+			$("#txtUsrHra").text(time1);
+			$("#txtNewUsrMnsg").text(sug);
+			$("#modalSugerencia").modal("toggle");
+			$("#msgCliente").hide();
+		} else {
+			$("#txtNoti").text("Porfavor ingresa la informacion solicitada");
+		}
+	})
 
+	$("#newMsg").hide();
+	$("#usrMnsgSeg").hide();
+	$("#typeMsg").hide();
+	$("#msgCliente").hide(); 
+	$("#frmNuevo").hide();
 });
